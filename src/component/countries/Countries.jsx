28f421cf1a5a70fc0fff04adb2e216a241f47a8d
Tushar12152 <tusharimran470@gmail.com>
2023-09-10 02:10:26 -0600

    react rest countries

--- src/component/countries/Countries.jsx
+++ src/component/countries/Countries.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Country from "../country/Country";
 
+
 const Countries = () => {
     const [rest,setRest]=useState([])
     useEffect(()=>{
@@ -14,15 +15,55 @@ const Countries = () => {
   }
   Data()  
     },[])
-    
-    return (
-        <div>
-             <h1 className="text-center font-semibold text-3xl">countries:{rest.length}</h1>
 
-             {
-                rest.map(country=><Country key={country.name?.common} country={country}></Country>)
-             }
-        </div>
+    const [visitedCountry,setVisitedCountry]=useState([])
+    const [visitedflag,setVisitedflag]=useState([])
+
+    const handleVisitedCountry=country=>{
+        
+       
+        const newVisitedCountries=[...visitedCountry,country.name.common];
+        setVisitedCountry(newVisitedCountries)
+        
+    }
+     const handleVisitedFlag=flag=>{
+        const flagList=[...visitedflag,flag.flags.png]
+          setVisitedflag(flagList)
+     }
+
+    return (
+
+        <>
+            <h1 className="text-center font-semibold text-3xl">countries:{rest.length}</h1>
+            <div>
+                 <h1>visited Flags:</h1>
+                
+                    {
+                        visitedflag.map((flag,idx)=> <img key={idx}className="w-10 mb-2" src={flag} alt="" />)
+                    }
+                 
+            </div>
+            <div>
+                <h1>visited country: <ul className="list-decimal list-disc">
+                    {
+                       visitedCountry.map(item=><li key={'5'}>{item}</li>) 
+                    }
+                </ul>
+
+
+                </h1>
+               
+            </div>
+            <div className="grid grid-cols-3 gap-6 w-[90%] mx-auto ">
+         
+
+         {
+            rest.map(country=><Country key={country.name?.common} country={country} handleVisitedCountry={handleVisitedCountry}
+            handleVisitedFlag={handleVisitedFlag}></Country>)
+         }
+    </div>
+        </>
+       
     );
 };
 
--- src/component/country/Country.jsx
+++ src/component/country/Country.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
 
 
-const Country = ({country}) => {
-    console.log(country)
-    const {name,flags,area,population,region}=country;
+const Country = ({country,handleVisitedCountry,handleVisitedFlag}) => {
+    const[visited,setVisited]=useState(false)
+    const handleClick=()=>{
+        setVisited(!visited)
+    }
+
+    const {name,flags,area,population,region,cca3}=country;
     return (
-        <div className="border-2 border-gray-400 rounded-xl mb-2 w-[50%]  mx-auto bg-slate-400 ">
+        <div className={`border-2 rounded-xl mb-2    ${visited?'bg-[violet]':'bg-slate-400' }`}>
             <h3 className="text-xl font-semibold p-4">Name:{name.common}</h3>
-            <img className="p-10 mx-auto" src={flags. png} alt="" />
-            <div className="border-2 border-yellow-200 w-[50%] mx-auto p-16 m-10">
+
+                <button className="btn mr-20 ml-5 mb-5"  onClick={()=>handleVisitedCountry(country)}>add visited</button>
+
+                <button className="btn ml-2"  onClick={()=>handleVisitedFlag(country)}>add visited Flag</button>
+
+            <button onClick={handleClick} className={`btn bg-[#e2d259] mx-10 ${visited&& 'bg-[#0b16e2] text-white'}`}>{visited? 'visited':"going"}</button>
+               {
+                visited?"yes i visited":'i want to visit'
+               }
+            <img className="p-10 mx-auto w-96" src={flags. png} alt="" />
+           
+
+            <div className="border-2 border-yellow-200 w-[70%] mx-auto rounded-xl p-5 ">
             <p className="font-semibold text-xl">Area:{area} Squre k.m</p>
             <h2 className="font-semibold text-xl">Population:{population}</h2>
             <h2 className="font-semibold text-xl">Region: {region}</h2>
+            <p>{cca3}</p>
             </div>
+            
 
         </div>
     );
